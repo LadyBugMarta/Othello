2395362aea9ae9bb3ce0c208fe9916c261a5cf27
Marta <martapabis.it@gmail.com>
2019-10-15 14:55:16 +0200

    write first game rules

--- Othello/Othello/GameRules.cs
+++ Othello/Othello/GameRules.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Othello
 {
-    class GameRules
+    public class GameRules
     {
+        public int SzerokoscPlanszy { get; private set; }
+        public int WysokoscPlanaszy { get; private set; }
+
+        private int[,] plansza;
+        public int NumerGraczaWykonujacegoNastepnyRuch { get; private set; } = 1;
+
+        private static int numerPrzeciwnika(int numerGracza)
+        {
+            return (numerGracza == 1) ? 2 : 1;
+        }
+
+        private bool czyWspolrzednePolaPrawidlowe(int poziomo, int pionowo)
+        {
+            return poziomo >= 0 && poziomo < SzerokoscPlanszy &&
+                pionowo >= 0 && pionowo < WysokoscPlanaszy;
+        }
+
+        private int PobierzStanPola(int poziomo, int pionowo)
+        {
+            if (!czyWspolrzednePolaPrawidlowe(poziomo, pionowo))
+                throw new Exception("Nieprawidlowe wspolrzedne pola");
+            return plansza[poziomo, pionowo];
+        }
     }
 }
