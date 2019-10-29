3a2626d47df7b1657ec09964478012f7780660db
Marta <martapabis.it@gmail.com>
2019-10-29 15:40:03 +0100

    the game rules implementation

--- Othello/Othello/GameRules.cs
+++ Othello/Othello/GameRules.cs
@@ -63,7 +63,106 @@ namespace Othello
             czyscPlansze();
 
             NumerGraczaWykonujacegoNastepnyRuch = numerGraczaRozpoczynajacego;
+            obliczLiczbyPol(); // licznik
         }
+
+        #endregion
+        #region Implementacja zasad gry
+        private void zmieńBiezacegoGracza()
+        {
+            NumerGraczaWykonujacegoNastepnyRuch = numerPrzeciwnika(NumerGraczaWykonujacegoNastepnyRuch);
+        }
+
+        // sprawdzenie czy argumenty metody należą do planszy && pole jest wolne
+        protected int PolozKamien(int poziomo, int pionowo, bool tylkoTest) 
+        {
+            // czy wsplółrzędne są prawidłowe
+            if (!czyWspolrzednePolaPrawidlowe(poziomo, pionowo))
+                throw new Exception("Nieprawidłowe współrzędne pola");
+
+            // czy pole nie jest już zajęte
+            if (plansza[poziomo, pionowo] != 0) return -1;
+
+            int ilePolPrzejetych = 0;
+
+            //pętla po 8 kierunkach przyjmuje wartości {-1,0,1}
+            for (int kierunekPoziomo = -1; kierunekPoziomo <= 1; kierunekPoziomo++) // sprawdzamy czy możemy przejąć pole w każdą stronę
+                for (int kierunekPionowo = -1; kierunekPionowo <= 1; kierunekPionowo++)
+                {
+                    // wymuszenie pominięcia przypadku, gdy obie zmienne są równe 0
+                    if (kierunekPoziomo == 0 && kierunekPionowo == 0) continue;
+
+                    // szukanie kamieni gracza w jednym z 8 kierunków
+                    int i = poziomo;
+                    int j = pionowo;
+                    bool znalezionyKamienPrzeciwnika = false;
+                    bool znalezionyKamienGraczaWykonujacegoRuch = false;
+                    bool znalezionePustePole = false;
+                    bool osiagnietaKrawedzPlanszy = false;
+                    do // przejmowanie pól
+                    {
+                        i += kierunekPoziomo;
+                        j += kierunekPionowo;
+                        if (!czyWspolrzednePolaPrawidlowe(i, j)) osiagnietaKrawedzPlanszy = true;
+                        if (!osiagnietaKrawedzPlanszy)
+                        {
+                            if (plansza[i, j] == NumerGraczaWykonujacegoNastepnyRuch) znalezionyKamienGraczaWykonujacegoRuch = true; // dopóki kładziemy kamień to wykonuje się pętla
+                            if (plansza[i, j] == 0) znalezionePustePole = true; // dopóki znajdziemy puste pole to wykonuje się pętla
+                            if (plansza[i, j] == numerPrzeciwnika(NumerGraczaWykonujacegoNastepnyRuch)) znalezionyKamienPrzeciwnika = true; // dopóki przeciwnik kładzie kamień to wykonuje się pętla
+                        }
+
+                    } while (!(osiagnietaKrawedzPlanszy || znalezionyKamienGraczaWykonujacegoRuch || znalezionePustePole)); // game over, bye bye
+
+                    // sprawdzenie warunku poprawności ruchu
+                    bool polozenieKamieniaJestMozliwe = znalezionyKamienPrzeciwnika && znalezionyKamienGraczaWykonujacegoRuch && !znalezionePustePole;
+                 
+                    // "odwrócenie" kamieni w przypadku spełnionego warunku
+                    if (polozenieKamieniaJestMozliwe)
+                    {
+                        int max_index = Math.Max(Math.Abs(i - poziomo), Math.Abs(j - pionowo)); // przejmujemy jak najwięcej pól
+
+                        if (!tylkoTest)
+                        {
+                            for (int index = 0; index < max_index; index++)
+                                plansza[poziomo + index * kierunekPoziomo, pionowo + index * kierunekPionowo] = NumerGraczaWykonujacegoNastepnyRuch;
+                        }
+                        ilePolPrzejetych += max_index - 1; // bez tego kamienia który kładzie 
+                    }
+                    obliczLiczbyPol(); // licznik
+
+                } // koniec pętli po kierunkach
+
+            // zmiana gracza, jeżeli ruch został wykonany 
+            if (ilePolPrzejetych > 0 && !tylkoTest)
+                zmieńBiezacegoGracza();
+            // zmienna ilePolPrzejętych nie uwzględnia dostawionego kamienia
+            return ilePolPrzejetych;
+            
+        }
+
+        // przeciążona wersja metody, sprawdza czy ruch jest możliwy
+        public bool PolozKamien(int poziomo, int pionowo)
+        {
+            return PolozKamien(poziomo, pionowo, false) > 0;
+        }
+        #endregion
+        // pola zajęte przez obu graczy
+        #region Obliczanie pól zajętych przez graczy
+
+        private int[] liczbyPol = new int[3]; // [puste pola], [liczbaPolGracz1], [liczbaPolGracz2]
+
+        private void obliczLiczbyPol()  
+        {
+            for (int i = 0; i < liczbyPol.Length; ++i)
+                liczbyPol[i] = 0;
+
+            for (int i = 0; i < SzerokoscPlanszy; i++)
+                for (int j = 0; j < WysokoscPlanaszy; ++j)
+                    liczbyPol[plansza[i, j]]++;
+        }
+        public int LiczbaPustychPol {  get { return liczbyPol[0]; } } // get - tylko do odczytu
+        public int LiczbaPolGracz1 {  get { return liczbyPol[1]; } }
+        public int LiczbaPolGracz2 {  get { return liczbyPol[2]; } }
         #endregion
     }
 }
